f8e974ef80bf9d634dab2c3a198e5ae196ea4c22
Svante Seleborg <svante@axantum.com>
2024-10-24 09:59:25 +0200

    :tada: Initial restructuring and refactoring (WIP)

---
+++ src/Slip39/MnemonicException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Slip39;
+
+public class MnemonicException(string message, Exception innerException) : Exception(message, innerException)
+{
+}
--- src/Slip39/WordList.cs
+++ src/Slip39/WordList.cs
@@ -1,6 +1,7 @@
+using Slip39.Properties;
+
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace Slip39;
@@ -15,22 +16,17 @@ public static class WordList
     {
         Words = LoadWordlist();
         WordIndexMap = Words
-            .Select((word, i) => new {word, i})
-            .ToDictionary(x => x.word, x => x.i);
+            .Select((word, i) => (word, i ))
+            .ToDictionary(t => t.word, t => t.i);
     }
 
     private static string[] LoadWordlist()
     {
-        string wordlistPath = Path.Combine(AppContext.BaseDirectory, "wordlist.txt");
-        string[] wordlist = File.ReadAllLines(wordlistPath)
-                           .Select(word => word.Trim())
-                           .ToArray();
+        string[] wordlist = Resources.WordList.Split(["\r\n", "\r", "\n"], StringSplitOptions.RemoveEmptyEntries);
 
-        return wordlist.Length != 1024
-            ? throw new InvalidOperationException(
-                $"The wordlist should contain 1024 words, but it contains {wordlist.Length} words."
-            )
-            : wordlist;
+        return wordlist.Length == 1024
+            ? wordlist
+            : throw new InvalidOperationException($"The wordlist should contain 1024 words, but it contains {wordlist.Length} words.");
     }
 
     public static int[] MnemonicToIndices(string mnemonic)
@@ -46,8 +42,4 @@ public static class WordList
             throw new MnemonicException($"Invalid mnemonic word {keyError.Message}.", keyError);
         }
     }
-}
-
-public class MnemonicException(string message, Exception innerException) : Exception(message, innerException)
-{
 }
